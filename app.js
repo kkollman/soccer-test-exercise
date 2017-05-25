87c7d718abebf9222a7af8029cbf5cda418771d2
Kamil Kollman <k.m.kollman@gmail.com>
2017-05-25 13:46:27 +0200

    added comments

--- app.js
+++ app.js
@@ -46,7 +46,7 @@
           }
           //select player that is described by current ID
           var currentPlayer = document.getElementById(`${playerId}`);
-          //set position values, derived from data
+          //set position values, derived from data and set as percentage
           var positionX = (currentFrame[i][1]*100), positionY = (currentFrame[i][2]*100);
           //set player position, using given coordinates as percentage of height/width of soccer field
           currentPlayer.style.cssText = `left: ${positionX}%; bottom: ${positionY}%;`;
@@ -55,7 +55,6 @@
         //setup the left counter: format time given in number of 100ms-frames
         var currentMinutes = Math.floor(time/600);
         var currentSeconds = Math.floor((time/10)%60);
-
         var setTimer = function() {
           //precede numbers with 0 if lesser than 10
           if (currentSeconds < 10) {
@@ -71,21 +70,23 @@
       //call function first time to ensure players are on the field already when match starts  ;)
       positionPlayers();
 
-
+      //define function that will be called when play button is clicked
       playPause = function() {
+        //check if the game is currently being played or paused
         if (button.classList.contains('paused')) {
+          //start passing time, if game was paused using data.interval as interval value and re-position player every frame; stop when time bar reaches maximum value
           playMatch = setInterval(function() {
                                   timeBar.value != timeBar.getAttribute('max') ? timeBar.value = parseInt(timeBar.value)+1 : clearInterval(playMatch);
                                   positionPlayers();
                                 }, frameTime);
             button.innerHTML = "Pause the game";
         } else if (!button.classList.contains('paused')) {
+          //pause the game if it wasn't
           clearInterval(playMatch);
           button.innerHTML = "Play the game";
         }
         button.classList.toggle('paused');
       }
-
       button.addEventListener('click', playPause);
       //every time the bar changes its postion (and therefore its value) it should trigger the action to set up the players in the current position on the field
       timeBar.addEventListener('change', positionPlayers);
